49f1df6085b60ab1800f3cb2384093d44d1e8065
filbert lim <zephiroth_filbert@live.com>
2017-06-01 17:35:35 +0700

    Add Transient

--- ProjectSepatu/Startup.cs
+++ ProjectSepatu/Startup.cs
@@ -16,6 +16,21 @@ using ProjectSepatu.DAL;
 using ProjectSepatu.Core.ProductProperties.ProductClass;
 using ProjectSepatu.DAL.ProductProperties.ProductClass;
 using ProjectSepatu.DAL.ProductProperties.BrandClass;
+using ProjectSepatu.DAL.ProductProperties.CabangMasterClass;
+using ProjectSepatu.DAL.ProductProperties.CustomerClass;
+using ProjectSepatu.DAL.ProductProperties.JenisPembayaranMasterClass;
+using ProjectSepatu.DAL.ProductProperties.KabupatenMasterClass;
+using ProjectSepatu.DAL.ProductProperties.KecamatanMasterClass;
+using ProjectSepatu.DAL.ProductProperties.PerhitunganPengirimanClass;
+using ProjectSepatu.DAL.ProductProperties.ProductColorClass;
+using ProjectSepatu.DAL.ProductProperties.ProductColorMasterClass;
+using ProjectSepatu.DAL.ProductProperties.ProductMasterClass;
+using ProjectSepatu.DAL.ProductProperties.ProductPictureClass;
+using ProjectSepatu.DAL.ProductProperties.ProductTypeMasterClass;
+using ProjectSepatu.DAL.ProductProperties.ProvinsiMasterClass;
+using ProjectSepatu.DAL.ProductProperties.SaranaPengirimanMasterClass;
+using ProjectSepatu.DAL.ProductProperties.TransaksiHeaderClass;
+using ProjectSepatu.DAL.ProductProperties.TransaksiListClass;
 
 namespace ProjectSepatu
 {
@@ -58,9 +73,27 @@ namespace ProjectSepatu
 
             services.AddMvc();
 
+
+            services.AddTransient<ProductRepo, ProductRepo>(); // Tidak dipakai, Hanya Untuk Testing Homepage
             // Add application services.
-            services.AddTransient<ProductRepo, ProductRepo>();
             services.AddTransient<BrandRepo, BrandRepo>();
+            services.AddTransient<CabangMasterRepo, CabangMasterRepo>();
+            services.AddTransient<CustomerRepo, CustomerRepo>();
+            services.AddTransient<JenisPembayaranMasterRepo, JenisPembayaranMasterRepo>();
+            services.AddTransient<KabupatenMasterRepo, KabupatenMasterRepo>();
+            services.AddTransient<KecamatanMasterRepo, KecamatanMasterRepo>();
+            services.AddTransient<PerhitunganPengirimanRepo, PerhitunganPengirimanRepo>();
+            services.AddTransient<ProductColorRepo, ProductColorRepo>();
+            services.AddTransient<ProductColorMasterRepo, ProductColorMasterRepo>();
+            services.AddTransient<ProductMasterRepo, ProductMasterRepo>();
+            services.AddTransient<ProductPictureRepo, ProductPictureRepo>();
+            services.AddTransient<ProductTypeMasterRepo, ProductTypeMasterRepo>();
+            services.AddTransient<ProvinsiMasterRepo, ProvinsiMasterRepo>();
+            services.AddTransient<SaranaPengirimanMasterRepo, SaranaPengirimanMasterRepo>();
+            services.AddTransient<TransaksiHeaderRepo, TransaksiHeaderRepo>();
+            services.AddTransient<TransaksiListRepo, TransaksiListRepo>();
+
+
             services.AddTransient<IEmailSender, AuthMessageSender>();
             services.AddTransient<ISmsSender, AuthMessageSender>();
         }
