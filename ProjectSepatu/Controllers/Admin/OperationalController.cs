61497936b039afd77b062bac317a6c4fa88d2231
Romualdus Angga <angga.de.wijaya@gmail.com>
2017-06-07 07:44:44 +0700

    agga operatio

---
+++ ProjectSepatu/Controllers/Admin/OperationalController.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectSepatu.Controllers.Admin
+{
+    public class OperationalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult TabMetodePembayaran()
+        {
+            return View();
+        }
+
+        public IActionResult TabJenisPembayaran()
+        {
+            return View();
+        }
+
+        public IActionResult TabBrand()
+        {
+            return View();
+        }
+
+        public IActionResult TabGender()
+        {
+            return View();
+        }
+
+        public IActionResult TabColor()
+        {
+            return View();
+        }
+
+        public IActionResult TabSize()
+        {
+            return View();
+        }
+
+        public IActionResult TabType()
+        {
+            return View();
+        }
+
+        public IActionResult TabBank()
+        {
+            return View();
+        }
+    }
+}
