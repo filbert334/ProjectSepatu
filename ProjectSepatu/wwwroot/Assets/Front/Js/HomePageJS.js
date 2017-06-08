716ae3dc599115d66cf8642668d550fbc39bea1b
suryadeo <surya.d.sindhunata@gmail.com>
2017-06-08 16:14:45 +0700

    js egie

--- ProjectSepatu/wwwroot/Assets/Front/Js/HomePageJS.js
+++ ProjectSepatu/wwwroot/Assets/Front/Js/HomePageJS.js
@@ -1,5 +1,6 @@
 ﻿
 function ToDetailsProduct(id) {
     alert(id)
-    window.location.replace("http://localhost:53293/Home/ProductDetails/" + id);
+    window.location.replace("/Home/ProductDetails/" + id);
+
 }
