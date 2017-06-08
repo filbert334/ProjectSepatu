f2d923d38981a509c3730af565940e7e0471afe0
maria clara egie <mariaclaraegie@outlook.com>
2017-06-08 11:53:37 +0700

    get data di homepage dan details product

--- ProjectSepatu/Controllers/HomeController.cs
+++ ProjectSepatu/Controllers/HomeController.cs
@@ -5,33 +5,67 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ProjectSepatu.DAL;
 using ProjectSepatu.DAL.ProductProperties.BrandClass;
+using ProjectSepatu.DAL.ProductProperties.ProductMasterClass;
+using ProjectSepatu.Models.HomeViewModels;
+using ProjectSepatu.Core.ProductProperties.ProductMasterClass;
 
 namespace ProjectSepatu.Controllers
 {
     public class HomeController : Controller
     {
         private BrandRepo _BrandRepo;
+        private ProductMasterRepo _ProductMasterRepo;
 
-        public HomeController(BrandRepo BrandRepo)
+        public HomeController(BrandRepo BrandRepo, ProductMasterRepo ProductMasterRepo)
         {
             _BrandRepo = BrandRepo;
+            _ProductMasterRepo = ProductMasterRepo;
         }
 
         public IActionResult Index()
         {
+          
             return View();
         }
         public IActionResult Coba()
         {
-            return View();
+            var ProductList = _ProductMasterRepo.GetAll().Where(i => i.IsHidden == false).ToList();
+            var viewModel = new HomePageViewModel();
+            var counter = 0;
+            if(ProductList!=null)
+            {
+                foreach (var item in ProductList)
+                {
+                    if (counter < 6)
+                    {
+                        viewModel.listProduct.Add(item);
+                    }
+                    counter++;
+
+                }
+            }
+            else
+            {
+                viewModel.listProduct = new List<ProductMaster>();
+            }
+            return View(viewModel);
         }
         public IActionResult List()
         {
+           
             return View();
         }
-        public IActionResult ProductDetails()
+        public IActionResult ProductDetails(int id= 0)
         {
-            return View();
+            var ProductSelect = _ProductMasterRepo.GetById(id);
+            if (ProductSelect != null)
+            {
+                return View(ProductSelect);
+            }
+            else
+            {
+                return View(new ProductMaster());
+            }
         }
         public IActionResult About()
         {
---
+++ ProjectSepatu/wwwroot/Assets/Front/Js/HomePageJS.js
@@ -0,0 +1,5 @@
+﻿
+function ToDetailsProduct(id) {
+    alert(id)
+    window.location.replace("http://localhost:53293/Home/ProductDetails/" + id);
+}
